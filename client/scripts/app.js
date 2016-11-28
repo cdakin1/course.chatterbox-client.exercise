1dfeeb96678199472bcda3e534dd531f0b26da00
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2016-11-28 17:27:39 -0600

    Some tests, no functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,72 @@
 // YOUR CODE HERE:
+var app = {};
+
+var friends = [];
+
+
+
+app.send = function (message) {
+  console.log('sending');
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: '',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+
+
+app.fetch = function (message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').html('');
+};
+
+app.renderMessage = function () {
+  $('#chats').prepend('<div class="chat"><span class="username">' + message.username + '</span>: ' + message.text + ' | ' + message.roomname + '</div>');
+};
+
+app.renderRoom = function () {
+  $('#roomSelect').prepend('<option> roomName </option>');
+};
+
+app.handleUsernameClick = function () {
+  friends.push();
+};
+
+app.handleSubmit = function () {
+
+};
+
+app.init = function () {
+  console.log('calling init');
+  $('.newMessage').on('click', function() {
+    console.log('clicked');
+    app.send();
+  });
+};
+
+app.init();
